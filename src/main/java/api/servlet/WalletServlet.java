ef7747a2d1b2014e1ee4a0ef03ddd29b6f5dfa22
KHAWKREAB/sitti <sittisukkung@hotmail.co.th>
2018-01-21 14:22:25 +0700

    user class,  product class

--- src/main/java/api/servlet/WalletServlet.java
+++ src/main/java/api/servlet/WalletServlet.java
@@ -46,8 +46,17 @@ public class WalletServlet extends HttpServlet {
 class User {
     private String id;
     private String name;
+    private double money;
 
-    public String getId() {
+    public double getMoney() {
+		return money;
+	}
+
+	public void setMoney(double money) {
+		this.money = money;
+	}
+
+	public String getId() {
         return id;
     }
 
@@ -63,4 +72,29 @@ class User {
         this.name = name;
     }
 
+}
+
+class Product {
+	private String productId;
+	private String productName;
+	private double productPrice;
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
 }
